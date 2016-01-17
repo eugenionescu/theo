cc21f0f27491d473340b6b5033ff523b00e6e5c0
Eugen <eugen@Eugens-MacBook.local>
2016-01-17 10:48:51 +0000

    Initial commit of theo project

---
+++ theo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for theo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theo.settings")
+
+application = get_wsgi_application()
